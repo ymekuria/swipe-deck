411483a2d2a32b84599486a3fd472bc4d45300a6
Yoni <ymekuria@gmail.com>
2017-05-04 21:38:25 -0700

    Added renderCard function to Deck component

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
 import Expo from 'expo';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 import Deck from './src/Deck';
 
+
+// Dummy array of card items for testing. 
+const CARDDATA = [
+  { id: 1, text: '#1', uri: 'https://unsplash.com/?photo=5EVrQKXPa5g' },
+  { id: 2, text: '#2', uri: 'https://unsplash.com/?photo=2h_i_BB_X2E' },
+  { id: 3, text: '#3', uri: 'https://unsplash.com/?photo=DxAV6se7QPM' },
+  { id: 4, text: '#4', uri: 'https://unsplash.com/?photo=trvP9JiYC1E' }
+];
+
 class App extends Component {
+
+  renderCard(item) {
+    const { id, text, uri } = item;
+
+    return (
+      <Card
+        key={id}
+        text={text}
+        image={{uri}}
+       >
+         <Button
+          icon={{ name: 'autorenew'}} 
+          backgroundColor="#03A9F4"
+          title="Button"  
+         /> 
+
+      </Card>   
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Deck />
+        <Deck 
+          cardData={CARDDATA} 
+          renderCard={this.renderCard}
+        />  
       </View>
     );
   }
--- src/Deck.js
+++ src/Deck.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'; 
+import { View, 
+  Text,
+  Animated,
+  PanResponder,
+  LayoutAnimation,
+  UIManager
+} from 'react-native'; 
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderCards() {
+      
+  return this.props.cardData.map((item) => {
+    return this.props.renderCard(item);
+  });
+
+  }
+
   render() {
     return(
       <View>
-        <Text>
-          Hello World!
-        </Text>
-       </View>   
-    )
+        {this.renderCards()}
+      </View>   
+    );
   }
 }
 

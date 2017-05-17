0cc2904babf8b9b793fb343980b21ff588ef576a
Yoni <ymekuria@gmail.com>
2017-05-16 22:19:51 -0700

    added onSwipeComplete method

--- src/Deck.js
+++ src/Deck.js
@@ -13,6 +13,7 @@ const SWIPE_THRESHOLD = .25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  s
   constructor(props) {
     super(props);
 
@@ -26,10 +27,13 @@ class Deck extends Component {
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
           console.log('swipe right');
+          this.swipeOffScreen('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
           console.log('swipe left');
+          this.swipeOffScreen('left');
         } else {
           // reset card to initial position
+          this.resetCardPositon();
         }          
       }
 
@@ -47,10 +51,24 @@ class Deck extends Component {
     Animated.timing(this.state.position, {
       toValue: { x, y: 0},
       duration: SWIPE_OUT_DURATION
-    }).start();     
+    }).start(() => this.onSwipeComplete(direction));     
   }
+
+  // this method is executed after a card is swiped
+  onSwipeComplete(direction) {
+   
+    // onSwipeRight/onSwipeLeft handlers can be defined and passed down as props to this component
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.topCardIndex]; // item is set to the card that was swiped
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    
+    // reset the position and increments the topCardIndex
+    this.state.position.setValue({ x: 0, y: 0 }); 
+    this.setState({ topCardIndex: this.state.topCardIndex + 1 });
+  }  
   
-  // resets card to initial position 
+  // animates card to initial position 
   resetCardPositon() {
     Animated.spring(this.state.position, {
       toValue: { x:0, y:0 }
@@ -88,18 +106,17 @@ class Deck extends Component {
             {renderCard(item)}
           </Animated.View>
         );
+      }
 
       // all other cards below the top are rendered without animation handlers  
       return (
-        <View>
+        <View
+          key={item.id}
+        >
           {renderCard(item)}
         </View>  
 
       );  
-
-      }
-
-
 
     });
   }
